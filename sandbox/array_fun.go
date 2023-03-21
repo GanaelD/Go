a97abce5ff46b334c36e0aebc7284c16d8c0ba86
GanaelD <ganael59320@gmail.com>
2023-03-21 12:35:15 +0100

    A few methods to learn to use slices

--- sandbox/array_fun.go
+++ sandbox/array_fun.go
@@ -1,10 +1,35 @@
 package sandbox
 
-import (
-	"fmt"
-)
+// Sum sums all the elements of a slice storing integers.
+func Sum(slice []int) int {
+	sum := 0
+	for _, nb := range slice {
+		sum += nb
+	}
+	return sum
+}
 
-func ArrayToSlice(arr []int) []int {
-	sli := arr[:]
-	fmt.Println(sli)
+// SumEven sums all the even elements of a slice storing integers.
+func SumEven(slice []int) int {
+	sum := 0
+	for _, nb := range slice {
+		if nb%2 == 0 {
+			sum += nb
+		}
+	}
+	return sum
+}
+
+// ReverseSlice returns a new slice that is the slice passed as an argument
+// with all of its elements reversed.
+func ReverseSlice(slice []int) []int {
+	length := len(slice)
+	res := make([]int, length)
+	for i := 0; i < length/2; i++ {
+		res[i], res[length-(i+1)] = slice[length-(i+1)], slice[i]
+	}
+	if length%2 != 0 {
+		res[length/2] = slice[length/2]
+	}
+	return res
 }
--- use_sandbox/main.go
+++ use_sandbox/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"example.com/sandbox"
 )
 
 func main() {
 	arr := [...]int{2, 23, 435}
-	sli := sandbox.ArrayToSlice(arr)
+	slice := arr[:]
+
+	sum := sandbox.Sum(slice)
+	evenSum := sandbox.SumEven(slice)
+
+	reversedSlice := sandbox.ReverseSlice(slice)
+
+	toPrint := fmt.Sprintf("The sum is %v, the even sum is %v and the slice in reverse order is %v", sum, evenSum, reversedSlice)
+
+	fmt.Println(toPrint)
 }
