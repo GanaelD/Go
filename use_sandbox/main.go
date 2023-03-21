35ca40572722ba2d027bc7585d4c1ff7f2a2eda4
GanaelD <ganael59320@gmail.com>
2023-03-21 14:11:48 +0100

    Use sandbox.ReverseSliceF in main.go

--- use_sandbox/main.go
+++ use_sandbox/main.go
@@ -10,12 +10,19 @@ func main() {
 	arr := [...]int{2, 23, 435}
 	slice := arr[:]
 
+	sliceString := make([]string, 10)
+	for i := 0; i < 10; i++ {
+		sliceString[i] = fmt.Sprintf("%v", i)
+	}
+
 	sum := sandbox.Sum(slice)
 	evenSum := sandbox.SumEven(slice)
 
 	reversedSlice := sandbox.ReverseSlice(slice)
+	reversedSliceString := sandbox.ReverseSliceF(sliceString) // equivalent to sandbox.ReverseSliceF[string](sliceString)
 
 	toPrint := fmt.Sprintf("The sum is %v, the even sum is %v and the slice in reverse order is %v", sum, evenSum, reversedSlice)
 
 	fmt.Println(toPrint)
+	fmt.Println(reversedSliceString)
 }
